d04abe579feee160b4667770efbb41bec8c32442
Elmer C <elmer.galo@outlook.com>
2019-09-23 22:12:35 -0400

    CREATE KEY PRESS EVENT FUNCTION
    - First condition checks A-Z keypress characters only
    
    - Second condition checks if key press letter is contain in the guess word
    
    - Third condition checks the key press letter position

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,16 +3,18 @@ var wordList = ["Earth", "Galaxy", "Jupiter", "Mars"];
 // FUNCTION WILL RETURN RANDOM WORD FROM wordList ARRAY
 function randomWord() {
 	var word = wordList[Math.floor(Math.random() * wordList.length)];
-	return word;
+	return word.toLowerCase();
 }
 
 function startGame() {
 	var guessWord = randomWord();
 	var currentWord = document.getElementById("currentWord");
-	currentWord.innerHTML = ""; 
+	var underscoreCharacter = document.querySelectorAll("#currentWord div");
+
+	currentWord.innerHTML = ""; //Removes previous guess word
 
 	// Loop will run as many times as the guessWord length and each time
-	// will create a Div element with an underscore character and then 
+	// will create a Div element with an underscore character and then
 	// append it to the HTML tag with id = currentWord
 	for (var i = 0; i < guessWord.length; i++) {
 		var character = document.createElement("div");
@@ -20,6 +22,20 @@ function startGame() {
 		div = document.getElementById("currentWord");
 		currentWord.appendChild(character);
 	}
+
+	document.onkeyup = function(event) {
+		var letter = event.key;
+
+		if (event.keyCode >= 65 && event.keyCode <= 90) {
+			if (guessWord.includes(letter)) {
+				for (let i = 0; i < guessWord.length; i++) {
+					if (guessWord[i] == letter) {
+						underscoreCharacter[i].textContent = letter;
+					}
+				}
+			}
+		}
+	};
 }
 
 startGame();
