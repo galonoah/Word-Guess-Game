539ddb9b4178d3d0707a88ac766260913e9a68f2
Elmer C <elmer.galo@outlook.com>
2019-09-23 21:48:42 -0400

    Create function to start the game and add loop to create underscore characters for guess word

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,25 @@
 var wordList = ["Earth", "Galaxy", "Jupiter", "Mars"];
 
+// FUNCTION WILL RETURN RANDOM WORD FROM wordList ARRAY
 function randomWord() {
 	var word = wordList[Math.floor(Math.random() * wordList.length)];
 	return word;
 }
 
-var guessWord = randomWord();
+function startGame() {
+	var guessWord = randomWord();
+	var currentWord = document.getElementById("currentWord");
+	currentWord.innerHTML = ""; 
+
+	// Loop will run as many times as the guessWord length and each time
+	// will create a Div element with an underscore character and then 
+	// append it to the HTML tag with id = currentWord
+	for (var i = 0; i < guessWord.length; i++) {
+		var character = document.createElement("div");
+		character.textContent = "_";
+		div = document.getElementById("currentWord");
+		currentWord.appendChild(character);
+	}
+}
+
+startGame();
