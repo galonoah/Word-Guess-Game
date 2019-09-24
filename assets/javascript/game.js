a7b0a699f7ada0fe425ba06a2248fae31979753a
Elmer C <elmer.galo@outlook.com>
2019-09-23 23:01:24 -0400

    Add condition for wrong guesses

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,6 @@
 var wordList = ["Earth", "Galaxy", "Jupiter", "Mars"];
 
-// FUNCTION WILL RETURN RANDOM WORD FROM wordList ARRAY
+// Function will return random word from wordList array
 function randomWord() {
 	var word = wordList[Math.floor(Math.random() * wordList.length)];
 	return word.toLowerCase();
@@ -8,9 +8,8 @@ function randomWord() {
 
 function startGame() {
 	var guessWord = randomWord();
+	console.log(guessWord);
 	var currentWord = document.getElementById("currentWord");
-	var underscoreCharacter = document.querySelectorAll("#currentWord div");
-
 	currentWord.innerHTML = ""; //Removes previous guess word
 
 	// Loop will run as many times as the guessWord length and each time
@@ -19,13 +18,20 @@ function startGame() {
 	for (var i = 0; i < guessWord.length; i++) {
 		var character = document.createElement("div");
 		character.textContent = "_";
-		div = document.getElementById("currentWord");
+		//div = document.getElementById("currentWord");
 		currentWord.appendChild(character);
 	}
+
+	var underscoreCharacter = document.querySelectorAll("#currentWord div");
+	var userGuessLetters = document.getElementById("userGuessLetters");
 
 	document.onkeyup = function(event) {
 		var letter = event.key;
 
+		// First condition checks A-Z keypress characters only
+		// Second condition checks if key press letter is contain in the guess word
+		// Third condition checks the key press letter position
+		// If first condition fails, letter is append it to userGuessedLetters
 		if (event.keyCode >= 65 && event.keyCode <= 90) {
 			if (guessWord.includes(letter)) {
 				for (let i = 0; i < guessWord.length; i++) {
@@ -33,8 +39,12 @@ function startGame() {
 						underscoreCharacter[i].textContent = letter;
 					}
 				}
+			} else {
+				var failLetter = document.createElement("div");
+				failLetter.textContent = event.key;
+				userGuessLetters.appendChild(failLetter);
 			}
-		}
+		} 
 	};
 }
 
